4c1b4079fe5f7c485a7a1d6bb45f50c6ef517f92
Jaideep1099 <47473829+Jaideep1099@users.noreply.github.com>
2020-04-20 12:52:32 +0530

    Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<LoginData> fetchLogin() async {
+  final response = await http.post("http://192.168.43.60:10000/vendor/login");
+
+  if (response.statusCode == 200) {
+    return LoginData.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to Login');
+  }
+}
+
+class LoginData {
+  final String sId;
+
+  LoginData({this.sId});
+
+  factory LoginData.fromJson(Map<String, dynamic> json) {
+    return LoginData(sId: json['login']);
+  }
+}
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var sgn = 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Stay Home Admin',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Stay Home Admin"),
+            ),
+            body: (sgn == 1)
+            ? Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(8),
+                  child: TextField(
+                    obscureText:true,
+                    decoration:InputDecoration(
+                      contentPadding: EdgeInsets.all(8))
+                  ),
+                ),
+            ]) 
+            : Column()));
+  }
+}
